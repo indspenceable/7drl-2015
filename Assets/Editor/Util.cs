a3552d298051fcf174bda9837793b225076adee8
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2017-03-06 16:48:37 -0500

    Fusses with the UI. Makes sure it looks the same regardless of the current resolution.

---
+++ Assets/Editor/Util.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Util {
+	[MenuItem("Edit/Reset Playerprefs")] 
+	public static void DeletePlayerPrefs() { PlayerPrefs.DeleteAll(); }
+}
